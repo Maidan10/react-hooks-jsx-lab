42b608f572b7dff809378c0b8ef4a3bb043f636e
Maidan10 <aidan.marube@gmail.com>
2025-01-21 00:43:51 +0300

    Added code

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div><h2>About Me</h2>
+  <p>
+    I am passionate about building user-friendly, accessible, and
+    performant web applications. I love learning new technologies and
+    solving complex problems.
+  </p>
+  <img src={image} alt="I made this" /></div>;
 }
 
 export default About;
